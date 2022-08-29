c11f451fe6b9d82f64d9af3a6f3d46eb307cfab8
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-29 12:05:44 -0700

    added placeholder card

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -4,19 +4,33 @@ var searchFormEl = document.querySelector("#search-form");
 
 function getParams() {
     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var searchParamsArr = document.location.search.split("&");
+    // var searchParamsArr = document.location.search.split("&");
 
     // Get the query and format values
-    var query = searchParamsArr[0].split("=").pop();
-    var format = searchParamsArr[1].split("=").pop();
+    // var query = searchParamsArr[0].split("=").pop();
+    // var format = searchParamsArr[1].split("=").pop();
 
-    searchApi(query, format);
+    // searchApi(query, format);
 }
 
 function printResults(resultObj) {
-    console.log(resultObj);
+    // console.log(resultObj);
 
-    // todo set up `<div>` to hold result content
+    // todo make resultTextEl = search input value
+
+    // todo set up `<div>` to hold resultContentEl
+    var cardBody = $("<div>").addClass("card-body");
+    var cardHeader = $("<h5>").text("title of article").addClass("card-title");
+    var cardDate = $("<p>").text("date of article").addClass("card-text");
+    var cardSummary = $("<p>").text("summary of article").addClass("card-text");
+    var cardButton = $("<a>").text("link to article").addClass("btn btn-primary");
+
+    cardBody.append(cardHeader, cardDate, cardSummary, cardButton)
+    $(resultContentEl).addClass("card");
+    $(resultContentEl).append(cardBody);
+
+
+    
 }
 
 function searchApi(query, format) {
@@ -32,6 +46,7 @@ function handleSearchFormSubmit(event) {
     event.preventDefault();
 }
 
-searchFormEl.addEventListener("submit", handleSearchFormSubmit);
+// searchFormEl.addEventListener("submit", handleSearchFormSubmit);
 
-getParams();
+// getParams();
+printResults();
