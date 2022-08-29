857eced9a36cc8aea1b5a3bfe25b8be77e28e7de
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-08-29 13:14:12 -0700

    modified searchinput & formatinput

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,9 @@ function handleSearchFormSubmit(event) {
 
 //  Code to get values
   // todo define searchInputVal = #search-input
+  var searchInputVal = document.querySelector("#search-input");
   // todo define formatInputVal =  #format-input
+ var formatInputVal = document.querySelector("#format-input");
 
 
   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
