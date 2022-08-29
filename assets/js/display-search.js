a186c12a5ef93bd527ad19d7f5343af5793f69f6
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-29 13:11:32 -0700

    fixed comment

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -4,13 +4,13 @@ var searchFormEl = document.querySelector("#search-form");
 
 function getParams() {
     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    // var searchParamsArr = document.location.search.split("&");
+    var searchParamsArr = document.location.search.split("&");
 
     // Get the query and format values
-    // var query = searchParamsArr[0].split("=").pop();
-    // var format = searchParamsArr[1].split("=").pop();
+    var query = searchParamsArr[0].split("=").pop();
+    var format = searchParamsArr[1].split("=").pop();
 
-    // searchApi(query, format);
+    searchApi(query, format);
 }
 
 function printResults(resultObj) {
@@ -47,7 +47,7 @@ function handleSearchFormSubmit(event) {
     event.preventDefault();
 }
 
-// searchFormEl.addEventListener("submit", handleSearchFormSubmit);
+searchFormEl.addEventListener("submit", handleSearchFormSubmit);
 
-// getParams();
-printResults();
+getParams();
+// printResults();
